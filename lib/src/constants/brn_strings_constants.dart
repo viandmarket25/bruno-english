3aa6efaf7b0ac66eee55220d8d3e0fe0dc697322
Winner Emetuche <45737118+viandmarket25@users.noreply.github.com>
2022-08-20 02:58:13 +0800

    Update brn_strings_constants.dart

--- lib/src/constants/brn_strings_constants.dart
+++ lib/src/constants/brn_strings_constants.dart
@@ -4,10 +4,10 @@ class BrnStrings {
   /// 加载获取 assets 资源需要
   static const String flutterPackageName = "bruno";
 
-  static const String getDateFailed = "获取数据失败，请重试";
-  static const String networkConnectError = "网络连接失败，检查后重试";
-  static const String noData = "暂无数据";
-  static const String noSearchData = "暂无搜索结果";
-  static const String clickPageRetry = "请点击页面重试";
-  static const String loadingContent = '加载中...';
+  static const String getDateFailed = "Failed, try again";
+  static const String networkConnectError = "Network failed, try again";
+  static const String noData = "No data yet";
+  static const String noSearchData = "no results";
+  static const String clickPageRetry = "click the page to try again";
+  static const String loadingContent = 'Loading...';
 }
